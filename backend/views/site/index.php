08557bba05da93f48ab5f42438a75db69c30a879
zulkarom <zulkarom@gmail.com>
2022-03-25 08:57:31 +0800

    j

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron text-center bg-transparent">
 
 
-        <p class="lead">E-SIJIL KUDA LASA</p>
+        <p class="lead">E-SIJIL KUDA LASAK</p>
 
         <p><a class="btn btn-lg btn-success" href="<?php echo Url::to(['/rider'])?>">RIDER LIST</a></p>
     </div>
