6b783f636400ae9afd258428d33e1cc6ef10e4e9
zulkarom <zulkarom@gmail.com>
2023-06-05 10:24:46 +0800

    check

--- backend/models/CompetitionPrint.php
+++ backend/models/CompetitionPrint.php
@@ -288,17 +288,72 @@ EOD;
 
 		<tr>
 		<td width="600" colspan="2">
-		<b>Deposit Vest:</b><br />
-		';
-		$html .= 'RM ' . $k->deposit_amount;
+
+		
+
+
+		<table>
+
+		<tr>
+		<td width="120"><b>Deposit Vest:</b><br />
+		RM ' . $k->deposit_amount . '</td>
+		<td>';
+		
+		$c1 = '';$c2 = '';
 		if($m->deposit_paid == 0){
-			$html .= ' (BELUM BAYAR)';
+
 		}else if($m->deposit_paid == 1){
-			$html .= ' (TELAH BAYAR)';
+			$c1 = '<span style="font-family:zapfdingbats;">3</span>';
 		}else if($m->deposit_paid == 2){
-			$html .= ' (TELAH DIKEMBALIKAN)';
+			$c1 = '<span style="font-family:zapfdingbats;">3</span>';
+			$c2 = '<span style="font-family:zapfdingbats;">3</span>';
 		}
-		$html .= '<br /><span style="font-size:8pt">Deposit Vest akan dipulangkan setelah vest diserahkan semula kepada penganjur</span>
+		$html .= '<br /><br /><table border="0">
+
+<tr>
+
+<td width="190">
+
+
+<table border="0" cellpadding="2">
+<tr>
+<td width="20" style="border:1px solid #000000">'.$c1.'</td>
+<td width="100"> <b>Paid</b></td>
+</tr>
+</table>
+<i>Verifikasi oleh:</i>
+
+
+</td>
+
+<td width="200">
+
+
+<table border="0" cellpadding="2">
+<tr>
+<td width="20" style="border:1px solid #000000">'.$c2.'</td><td width="100"> <b>Return</b></td>
+</tr>
+</table>
+<i>Verifikasi oleh:</i>
+
+</td>
+
+
+</tr>
+
+</table>
+		
+		
+		</td>
+		</tr>
+
+		</table>';
+
+
+
+
+
+		$html .= '<br /><br /><br /><span style="font-size:8pt">Deposit Vest akan dipulangkan setelah vest diserahkan semula kepada penganjur</span>
 		</td>
 
 		</tr>
