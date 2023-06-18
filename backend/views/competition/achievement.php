4d928c0d6fa851de709cf658c4f1258a6cde3877
zulkarom <zulkarom@gmail.com>
2023-06-18 14:42:19 +0800

    g

--- backend/views/competition/achievement.php
+++ backend/views/competition/achievement.php
@@ -18,6 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="card">
         <div class="card-body">
+        LINK: <?=Html::a('https://pusatko.umk.edu.my/ekudalasak/ecert', 'https://pusatko.umk.edu.my/ekudalasak/ecert', ['target' => 'blank'])?>
         <?=$this->render('_search_achieve', ['model' => $searchModel])?>       
 
 <div class="competition-index">
